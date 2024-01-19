5cd3a9ea309145ede3f6433e874867822ca9f46c
Benpeake <dearbenp@gmail.com>
2024-01-19 18:39:07 +0000

    adding contact section with link hover animation

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -5,6 +5,26 @@ function Main()
 
     return(
         <>
+            <section className="bio-section">
+                <div>
+                    <h1>Front-end focused full-stack developer with a design background, passionate about crafting creative and useful digital experiences.</h1>
+                </div>
+                <div></div>
+            </section>
+            <section className="contact-section">
+                <div className="contact-detail-container">
+                    <p className="bold">Contact</p>
+                    <p><a className="link" href="mailto:benpeake.dev@gmail.com" target="_blank">benpeake.dev@gmail.com</a></p>
+                </div>
+                <div className="contact-detail-container">
+                    <p className="bold">Github</p>
+                    <p><a className="link" href="https://github.com/Benpeake" target="_blank" rel="noopener">@benpeake</a></p>
+                </div>
+                <div className="contact-detail-container">
+                    <p className="bold">Location</p>
+                    <p>Bath, UK</p>
+                </div>
+            </section>
         </>
     )
 
